bd9704f67c49e48393595610b8cdf716146869a5
20MM <114557868+maksmil@users.noreply.github.com>
2022-10-05 09:14:37 +0200

    program.cs

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zad 1
+            /*
+            int a = int.Parse(Console.Read.int());
+            int b = int.Parse(Console.Read.int());
+            Console.Write().ine(a * a + Math.Pow(b, 2));
+            */
+
+            // Zad 2
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b) * (a + b));
+            */
+
+            // Zad 3
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Pow((a-b), 3));
+            Console.ReadKey();
+            */
+
+            // Zad 4
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * b * c);
+            Console.ReadKey();
+            */
+
+            // Zad 5
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(2 * (a + b) / 5);
+            Console.ReadKey();
+            */
+
+            // Zad 6
+            /*
+            int brutto = int.Parse(Console.ReadLine());
+            Console.WriteLine(brutto / 1.23);
+            Console.ReadKey();
+            */
+
+            // Zad 7
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a%b);
+            Console.ReadKey();
+            */
+
+
+
+
+        }
+    }
+}
+          
+            
